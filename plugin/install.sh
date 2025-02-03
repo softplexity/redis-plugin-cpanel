af9555f4f691266d1f4fdf1b645d32e9136df822
Iulian Lixandru <iulian.lixandru12@gmail.com>
2025-02-03 19:57:38 +0200

    Initial Release

--- plugin/install.sh
+++ plugin/install.sh
@@ -3,7 +3,7 @@ mkdir /usr/local/cpanel/base/frontend/jupiter/redis_plugin
 cd /usr/local/cpanel/base/frontend/jupiter/redis_plugin
 
 echo "Downloading Redis cPanel Plugin..."
-wget -q https://github.com/atikrahmanbd/redis-cpanel-plugin/archive/main.zip -O Redis_Plugin_Package.zip
+wget -q https://github.com/softplexity/redis-cpanel-plugin/archive/main.zip -O Redis_Plugin_Package.zip
 
 # Extract Archive ZIP
 echo "Extracting Plugin..."
@@ -11,12 +11,6 @@ unzip Redis_Plugin_Package.zip
 
 # Moving To Plugin Residence
 mv redis-cpanel-plugin-main/plugin/* ./
-
-## No Longer Needed - Code Replaced By RedisManager.php Class
-# mv redis-cpanel-plugin-main/manage_redis.sh /usr/local/bin/manage_redis.sh
-
-# Make The Script Executable
-# sudo chmod +x /usr/local/bin/manage_redis.sh
 
 # Register Plugin with cPanel
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/jupiter/redis_plugin --theme jupiter
