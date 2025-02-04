91409e25024663e1e75b524d82117e06a91c8fe3
Iulian Lixandru <iulian.lixandru12@gmail.com>
2025-02-04 15:18:02 +0200

    fix install

--- plugin/install.sh
+++ plugin/install.sh
@@ -3,14 +3,14 @@ mkdir /usr/local/cpanel/base/frontend/jupiter/redis_plugin
 cd /usr/local/cpanel/base/frontend/jupiter/redis_plugin
 
 echo "Downloading Redis cPanel Plugin..."
-wget -q https://github.com/softplexity/redis-cpanel-plugin/archive/main.zip -O Redis_Plugin_Package.zip
+wget -q https://github.com/softplexity/redis-plugin-cpanel/archive/refs/heads/main.zip -O Redis_Plugin_Package.zip
 
 # Extract Archive ZIP
 echo "Extracting Plugin..."
 unzip Redis_Plugin_Package.zip
 
 # Moving To Plugin Residence
-mv redis-cpanel-plugin-main/plugin/* ./
+mv redis-plugin-cpanel/plugin/* ./
 
 # Register Plugin with cPanel
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/jupiter/redis_plugin --theme jupiter
@@ -19,10 +19,10 @@ mv redis-cpanel-plugin-main/plugin/* ./
 #Cleanup By Removing Packages
 echo "Cleaning Up..."
 rm -vf Redis_Plugin_Package.zip
-rm -rvf redis-cpanel-plugin-main
+rm -rvf redis-plugin-cpanel
 cd -
 cd ../
-rm -rvf redis-cpanel-plugin
+rm -rvf redis-plugin-cpanel
 
 # Fix Permissions
 echo "Finalizing Permissions..."
